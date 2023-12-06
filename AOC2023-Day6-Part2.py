056fa4ee3d1c1ec4a64e3741326241522eb51d11
kogosd <dkogos@gmail.com>
2023-12-06 10:25:10 -0500

    Create AOC2023-Day6-Part2.py

---
+++ AOC2023-Day6-Part2.py
@@ -0,0 +1,15 @@
+from math import *
+
+def calc(t, r):
+    d = sqrt((t/2)**2 -r)
+    x1 = t/2 + d
+    x2 = t/2 - d
+    return (floor(x1) - ceil(x2) + 1)
+
+t = 71530
+r = 940200
+print(calc(t,r))
+
+t = 51926890
+r = 222203111261225
+print(calc(t,r))
